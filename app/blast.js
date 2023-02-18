fb0787a18b4c11afeb11c8ba761042c945426c4c
raflylesmana3003 <raflylesmana111@gmail.com>
2023-02-19 01:22:18 +0700

    update logs

--- app/blast.js
+++ app/blast.js
@@ -8,7 +8,7 @@ let qr = ''
 
 const clientBlast = new Client({
     authStrategy: new LocalAuth({ clientId: "client-blast" }),
-    puppeteer: {headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']},
+    puppeteer: { headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] },
 });
 
 app.get('/log', multer().any(), async (request, response) => {
@@ -16,7 +16,13 @@ app.get('/log', multer().any(), async (request, response) => {
 });
 
 
-app.post('/connection', multer().any(), async (request, response) => {
+// This code is used to get the connection status of the client.
+// The connection status is used to determine if the client is connected to the server.
+// If the connection status is true, the client is connected to the server.
+// If the connection status is false, the client is not connected to the server.
+// The connection status is stored in the variable 'connection'.
+// The connection status is returned to the user.
+app.get('/connection', multer().any(), async (request, response) => {
     connection = ''
     clientBlast.getState().then((data) => {
         return response.status(200).json({ connection: data });
@@ -24,94 +30,75 @@ app.post('/connection', multer().any(), async (request, response) => {
 });
 
 
+/**
+ * Destroy session-client-blast
+ */
 app.get('/destroy', multer().any(), async (request, response) => {
     const dir = '.wwebjs_auth/session-client-blast'
     await fs.rm(dir, { recursive: true, force: true }, err => {
         if (err) {
             throw err
         }
-    
+
         console.log(`${dir} is deleted!`)
     })
-    io.emit('wa_blast_log', `${now} : Destroy`);
+    logger.warn("WA SESSION | Wa session destroyed");
     clientBlast.destroy();
     clientBlast.initialize();
     return response.status(200).send('Destroyed');
 });
 
+// Initialize the client blast.
 clientBlast.initialize();
 
-
+// Listen for QR Code
+clientBlast.on('qr', qr => {
+    // Convert QR Code to string
+    QRCode.toString(qr, { type: 'terminal' }, function (err, url) {
+        // Convert QR Code to dataURL
+        QRCode.toDataURL(qr, function (err, url) {
+            // Save QR Code to file
+            fs.writeFile('qrCode.txt', url, function (err) {
+                if (err) throw err;
+                console.log('QR Code saved to file');
+            });
+            // Send QR Code to client
+            io.emit("wa_blast_qr", url);
+            // Log QR Code received
+            io.emit('wa_blast_log', `QR Code received`);
+            logger.info("QR CODE | New QR Code received");
+        });
+    });
+});
+// When the client receives the loading_screen event, emit a socket message
+// with the data it receives from the clientBlast loading_screen event
 clientBlast.on('loading_screen', (percent, message) => {
     io.emit("wa_blast_log", `${now} : Loading ${percent}% ${message}`);
 });
 
-clientBlast.on('qr', qr => {
-    QRCode.toString(qr,{type:'terminal'}, function (err, url) {
-        QRCode.toDataURL(qr, function (err, url) {
-            io.emit("wa_blast_qr", url);
-            io.emit('wa_blast_log', `QR Code received`);
-        })
-      })
-});
-
+// Code that listens for a ready event from the WhatsApp client, and sends a message to the client to log the event to the client.
 clientBlast.on('ready', () => {
+    logger.info("WA SESSION | Connected to WhatsApp");
     io.emit("wa_blast_log", `${now} : WhatsApp is ready!`);
 });
 
+// Code that listens for an authenticated event from the WhatsApp client, and sends a message to the client to log the event to the client.
 clientBlast.on('authenticated', () => {
+    logger.info("WA SESSION | WhatsApp is authenticated!");
     io.emit("wa_blast_log", `${now} : Whatsapp is authenticated!`);
 });
 
-clientBlast.on('auth_failure', function(session) {
+// Code that listens for an auth_failure event from the WhatsApp client, and sends a message to the client to log the event to the client.
+clientBlast.on('auth_failure', function (session) {
+    logger.error("WA SESSION | Auth failure, restarting...");
     io.emit('wa_blast_log', `${now} : Auth failure, restarting...`);
 });
 
-clientBlast.on('disconnected', function() {
+clientBlast.on('disconnected', function () {
+    logger.error("WA SESSION | Disconnected, Please login again");
     clientBlast.destroy();
     clientBlast.initialize();
     return response.status(200).send('session destroyed');
-  });
-
-let download = function(uri, filename, callback){
-    request.head(uri, function(err, res, body){
-      request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-    });
-    return response.status(200).send('request received');
-  };
-/**
- *  this function is used for sending
- *  you can use by hit endpoint `/send`
- * 
- * Args(form body) :
- * @param {string} number - user wa phone number
- * @param {string} message - message you want to send
-*/
-app.post('/send/media', multer().any(), async (request, response) => {
-    let message = request.body.message;
-    let attachmentUrl = request.body.attachmentUrl;
-    let attachmentName = request.body.attachmentName;
-    let phoneNumber = request.body.number;
-
-    if(phoneNumber === 'status@broadcast'){
-        return response.status(200).send('brodcast received');
-    }
-    // check for number in request
-    if (!phoneNumber) {
-        return response.status(400).send('Number not found');
-    }
-    number = phoneNumber.includes('@c.us') ? phoneNumber : `${phoneNumber}@c.us`;
-    // check for is number is registered
-    const registered =  await clientBlast.isRegisteredUser(number);
-    if(!registered){
-        return response.status(400).send('Invalid number');    
-    }
-    await download(attachmentUrl, attachmentName, function(){
-    console.log('done');
-    let attachment =  MessageMedia.fromFilePath(attachmentName);
-    clientBlast.sendMessage(number, attachment,{caption:message});
-    return response.status(200).send('message sended');
-    });
 });
 
 /**
@@ -125,100 +112,26 @@ app.post('/send/media', multer().any(), async (request, response) => {
 app.post('/send/message', multer().any(), async (request, response) => {
     let message = request.body.message;
     let phoneNumber = request.body.number;
-
-    if(phoneNumber === 'status@broadcast'){
-        return response.status(200).send('brodcast received');
-    }
-    // check for number in request
-    if (!phoneNumber) {
-        return response.status(400).send('Number not found');
-    }
-    number = phoneNumber.includes('@c.us') ? phoneNumber : `${phoneNumber}@c.us`;
-    // check for is number is registered
-    const registered =  await clientBlast.isRegisteredUser(number);
-    if(!registered){
-        return response.status(400).send('Invalid number');    
-    }
-    await clientBlast.sendMessage(number, message);
-    return response.status(200).send('message sended');
-});
-
-/**
- *  this function is used for sending
- *  you can use by hit endpoint `/send`
- * 
- * Args(form body) :
- * @param {string} number - user wa phone number
- * @param {string} message - message you want to send
-*/
-app.post('/send/button', multer().any(), async (request, response) => {
-    let message = request.body.message;
-    let phoneNumber = request.body.number;
-    let title = request.body.title;
-    let footer = request.body.footer;
-    let buttons = JSON.parse(request.body.buttons);
-
-    if(phoneNumber === 'status@broadcast'){
-        return response.status(200).send('brodcast received');
-    }
-    // check for number in request
-    if (!phoneNumber) {
-        return response.status(400).send('Number not found');
-    }
-    number = phoneNumber.includes('@c.us') ? phoneNumber : `${phoneNumber}@c.us`;
-    // check for is number is registered
-    const registered =  await clientBlast.isRegisteredUser(number);
-    if(!registered){
-        return response.status(400).send('Invalid number');    
+    
+    try {
+        if (phoneNumber === 'status@broadcast') {
+            return response.status(200).send('brodcast received');
+        }
+        // check for number in request
+        if (!phoneNumber) {
+            return response.status(400).send('Number not found');
+        }
+        number = phoneNumber.includes('@c.us') ? phoneNumber : `${phoneNumber}@c.us`;
+        // check for is number is registered
+        const registered = await clientBlast.isRegisteredUser(number);
+        if (!registered) {
+            return response.status(400).send('Invalid number');
+        }
+        await clientBlast.sendMessage(number, message);
+        return response.status(200).send('message sended');
+    } catch (error) {
+        logger.error("SEND MESSAGE | Error : " + error, phoneNumber + " | " + message );
+        return response.status(500).send('error');
     }
     
-    const buttons_reply = new Buttons(message, buttons, title, footer)
-    
-    // send to number
-    for (const component of [buttons_reply]) await clientBlast.sendMessage(number, component);
-
-    return response.status(200).send('message sended');
 });
-
-/**
- *  this function is used for sending
- *  you can use by hit endpoint `/send`
- * 
- * Args(form body) :
- * @param {string} number - user wa phone number
- * @param {string} message - message you want to send
-*/
-app.post('/send/list', multer().any(), async (request, response) => {
-    let message = request.body.message;
-    let phoneNumber = request.body.number;
-    let cta = request.body.cta;
-    let title = request.body.title;
-    let footer = request.body.footer;
-    let buttons = JSON.parse(request.body.buttons);
-
-
-    if(phoneNumber === 'status@broadcast'){
-        return response.status(200).send('brodcast received');
-    }
-    // check for number in request
-    if (!phoneNumber) {
-        return response.status(400).send('Number not found');
-    }
-    number = phoneNumber.includes('@c.us') ? phoneNumber : `${phoneNumber}@c.us`;
-    // check for is number is registered
-    const registered =  await clientBlast.isRegisteredUser(number);
-    if(!registered){
-        return response.status(400).send('Invalid number');    
-    }
-
-    const section = {
-    title: title,
-    rows: buttons
-    };
-
-    const list = new List(message, cta, [section], title, footer)
-    await clientBlast.sendMessage(number, list);
-    return response.status(200).send('message sended');
-});
-
-
--- app/index.js
+++ app/index.js
@@ -18,6 +18,26 @@ const io = require("socket.io")(server, {
   },
 });
 
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
+const moment = require('moment-timezone');
+moment.tz.setDefault('Asia/Jakarta');
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const timestampJakarta = moment(timestamp).format('YYYY-MM-DD HH:mm:ss');
+  return `${timestampJakarta} | ${level.toUpperCase()} | ${message}`;
+});
+
+const logger = createLogger({
+    format: combine(
+      timestamp(),
+      myFormat
+    ),
+    transports: [
+        new transports.File({ filename: 'log/info.log', level: 'silly' })
+    ],
+});
+
 // socket Router
 function SocketRouter(io) {
   const router = express.Router();
@@ -38,8 +58,38 @@ eval(fs.readFileSync('app/blast.js')+'');
 
 // socket connection
 io.on("connection", (socket) => {
+  logger.info("WEB SOCKET CONNECTION | New client web socket connected " + socket.id);
   console.log(socket.id);
+  fs.readFile('qrCode.txt', 'utf8', function (err, data) {
+    if (err) logger.error("QR CODE | Error read local QA code " + err);
+    // Send QR Code to client
+    io.emit("wa_blast_qr", data);
+  });
 });
+
+setInterval(() => {
+  fs.readFile('log/info.log', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const lines = data.trim().split('\n');
+    const logs = lines
+      .map(line => {
+        const [timestamp, level, group, message] = line.split(' | ');
+        return { timestamp, level, group, message };
+      })
+      .filter(log => {
+        const today = moment().format('YYYY-MM-DD');
+        return log.timestamp.startsWith(today) && log.group.toUpperCase() !== 'WEB SOCKET CONNECTION';
+      })
+      .sort((a, b) => {
+        return b.timestamp.localeCompare(a.timestamp);
+      })
+      .slice(0, 100);
+    io.emit('log', logs);
+  });
+}, 1000);
 
 server.listen(port, () => {
   console.log(`Now listening on port ${port}`); 
